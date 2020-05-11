c3e05c3aad3643329e8fcb66c8af299e8f14dac6
divious1 <josehelps@gmail.com>
2020-05-11 14:40:00 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
 # git-wild-hunt
-A tool to hunt for credentials in github wild AKA git*hunt
+A tool to hunt for credentials in the Github wild AKA git*hunt
+![](static/wildhunt.jpg)
+
+
+### Usage
+
+```
+usage: git-wild-hunt.py [-h] -s SEARCH [-c CONFIG] [-v]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SEARCH, --search SEARCH
+                        search to execute
+  -c CONFIG, --config CONFIG
+                        config file path
+  -v, --version         shows current git-wild-hunt version
+```
+
+### Installation 
+
+* requirements:     `virtualenv, python3`
+
+1. `git clone https://github.com/d1vious/git-wild-hunt && cd git-wild-hunt` clone project and cd into the project dir
+2. `pip install virtualenv && virtualenv -p python3 venv && source venv/bin/activate && pip install -r requirements.txt` create virtualenv and install requirements
+
+Continue to [configuring](#configuration) a github API key
+
+### Configuration [`git-wild-hunt.conf`]()
+
+Make sure you set a Github token if you need to create one for your account follow [these](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line) instructions. 
+
+```
+[global]
+github_token = ''
+# github token for searching
+
+output = results.json
+# stores matches in JSON here
+
+log_path = git-wild-hunt.log
+# Sets the log_path for the logging file
+
+log_level = INFO
+# Sets the log level for the logging
+# Possible values: INFO, ERROR
+
+regexes = regexes.json
+# regexes to check the git wild hunt search against
+```
+
+### Github search examples
+
+* Searching for Jenkins credentials
+* Find GCP JWT token files
+* Find secrets in .circleci 
+* Find AWS secrets
+* Find ...
+
+
+### [`regexes.json`]()
+This file contains all the regexes that will be used to check against the raw content filed returned for a [search](#github-search-examples). Feel free to add/modify and include any specific ones that match the credential you are trying to find. 
+
+List of checked credentials via regex:
+
+*   AWS API Key
+*   Amazon AWS Access Key ID
+*   Amazon MWS Auth Token
+*   Facebook Access Token
+*   Facebook OAuth
+*   Generic API Key
+*   Generic Secret
+*   GitHub
+*   Google (GCP) Service-account
+*   Google API Key
+*   Google Cloud Platform API Key
+*   Google Cloud Platform OAuth
+*   Google Drive API Key
+*   Google Drive OAuth
+*   Google Gmail API Key
+*   Google Gmail OAuth
+*   Google OAuth Access Token
+*   Google YouTube API Key
+*   Google YouTube OAuth
+*   Heroku API Key
+*   MailChimp API Key
+*   Mailgun API Key
+*   PGP private key block
+*   Password in URL
+*   PayPal Braintree Access Token
+*   Picatic API Key
+*   RSA private key
+*   SSH (DSA) private key
+*   SSH (EC) private key
+*   Slack Token
+*   Slack Webhook
+*   Square Access Token
+*   Square OAuth Secret
+*   Stripe API Key
+*   Stripe Restricted API Key
+*   Twilio API Key
+*   Twitter Access Token
+*   Twitter OAuth
+
+### Author
+
+Jose Hernandez [@d1vious]()
+Rod Soto [@rsoto]()
+
+### Credits & References
+
+Inspiration [shhgit](https://github.com/eth0izzle/shhgit/)
---
+++ git-wild-hunt.py
@@ -0,0 +1,207 @@
+#!/usr/bin/python
+
+import argparse
+import requests
+import json
+import re
+import os
+import sys
+import datetime
+from pathlib import Path
+from modules.CustomConfigParser import CustomConfigParser
+from modules import logger
+
+VERSION = 1
+
+def load_regexes(regexes_path):
+    reg = dict()
+    with open(os.path.join(os.path.dirname(__file__), regexes_path), 'r') as f:
+        regexes = json.loads(f.read())
+
+    return regexes
+
+def search_github(github_token, search):
+    results = []
+    h = {"Authorization":"token "+ github_token}
+    url = "https://api.github.com/search/code?per_page=100&q=" + search
+    #extension:yml+path:.circleci+filename:config+language:YAML
+    r = requests.get( url, headers=h, timeout=30)
+    result = r.json()
+    log.info("total results: {}".format(result['total_count']))
+    
+    # first check for rate limit
+    if 'documentation_url' in result:
+        if result['documentation_url'] == "https://developer.github.com/v3/#abuse-rate-limits":
+            r = requests.get('https://api.github.com/rate_limit', headers=h, timeout=30)
+            log.error("we have hit githubs rate limit, cool off, return on: {}..exiting".format(r.headers.get('X-RateLimit-Reset')))
+            sys.exit(1)
+
+    if 'items' in result:
+        for i in result['items']:
+            results.append(i)
+
+    # check if there is pagination
+    link = r.headers.get('link')
+    if link is None:
+        log.info("all done processing results")
+        return result
+    next_url = find_next(r.headers['link'])
+    # process pagination of results
+    if next_url is None:
+        log.info("all done processing results")
+        return results
+    else:
+        results = process_pages(github_token, next_url, results)
+        log.info("all done processing results")
+        return results
+
+def process_pages(github_token, url, results):
+    log.info("processing page: {0}".format(url[-1]))
+    # limiting for testing
+    if int(url[-1]) >= 4:
+        return results
+
+    h = {"Authorization":"token "+ github_token}
+    r = requests.get( url, headers=h, timeout=30)
+    result = r.json()
+    
+    # first check for rate limit
+    if 'documentation_url' in result:
+        if result['documentation_url'] == "https://developer.github.com/v3/#abuse-rate-limits":
+            log.error("we have git githubs rate limit, cool off, return on: {} ..exiting".format(r.headers.get('X-RateLimit-Reset')))
+            sys.exit(1)
+    # process results if we haven't hit any
+    if 'items' in result:
+        for i in result['items']:
+            results.append(i)
+
+    # check if there is pagination
+    link = r.headers.get('link')
+    if link is None:
+        return result
+    next_url = find_next(r.headers['link'])
+    # process pagination of results
+    if next_url is None:
+        log.info("all done processing results")
+        return results
+    return process_pages(github_token, next_url, results)
+        
+
+
+# given a link header from github, find the link for the next url which they use for pagination
+def find_next(link):
+    for l in link.split(','):
+        a, b = l.split(';')
+        if b.strip() == 'rel="next"':
+            return a.strip()[1:-1]
+
+def rawurl( result ):
+    if 'html_url' in result:
+        raw_url = result['html_url'];
+        raw_url = raw_url.replace( 'https://github.com/', 'https://raw.githubusercontent.com/' )
+        raw_url = raw_url.replace( '/blob/', '/' )
+        return raw_url
+    else:
+        raw_url = None
+        return raw_url
+
+def getcode( url ):
+    # print( url )
+    try:
+        r = requests.get( url, timeout=10 )
+        return r.text
+    except: 
+        log.error("timeout requesting url: {0} .. continuing".format(url))
+        code = None
+        return code 
+
+def findleaks(conf, regexes):
+    match_creds = []
+    match_details = dict()
+    url = rawurl(conf)
+    if url:
+        code = getcode(url)
+    else:
+        log.error("could not get raw content for {}".format(json.dumps(conf, indent=2)))
+        code = None
+    if code:
+        for check, regex in regexes.items():
+            matches = re.findall(regex, code)
+            if matches:
+                match_details['url'] = url
+                match_details['check'] = check
+                match_details['matches']  = matches
+                match_details['timestamp']  = str(datetime.datetime.utcnow().isoformat())
+                match_creds.append(match_details)
+                log.warning("url: {0}\ncheck: {1} matches: {2}".format(url, check, matches))
+    return match_creds
+
+def write_leaks(leaks, output_path):
+    try:
+        with open(output_path, 'a') as outfile:
+            json.dump(leaks, outfile)
+    except Exection as e:
+        log.error("writing result file: {0}".format(str(e)))
+
+if __name__ == "__main__":
+    # grab oarguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search", required=True, default="", help="search to execute")
+    parser.add_argument("-c", "--config", required=False, default="git-wild-hunt.conf", help="config file path")
+    parser.add_argument("-v", "--version", default=False, action="store_true", required=False,
+                        help="shows current git-wild-hunt version")
+
+    # parse them
+    args = parser.parse_args()
+    ARG_VERSION = args.version
+    config = args.config
+    search = args.search
+    
+    # needs config parser here
+    tool_config = Path(config)
+    if tool_config.is_file():
+        print("git-wild-hunt is using config at path {0}".format(tool_config))
+        configpath = str(tool_config)
+    else:
+        print("ERROR: git-wild-hunt failed to find a config file at {0} or {1}..exiting".format(tool_config))
+        sys.exit(1)
+
+    # Parse config
+    parser = CustomConfigParser()
+    config = parser.load_conf(configpath)
+
+    log = logger.setup_logging(config['log_path'], config['log_level']
+)
+    log.info("INIT - git-wild-hunt v" + str(VERSION))
+
+    if ARG_VERSION:
+        log.info("version: {0}".format(VERSION))
+        sys.exit(0)
+
+    regexes = load_regexes(config['regexes'])
+
+    if config['github_token']:
+        results = search_github(config['github_token'], search)
+    else:
+        print("ERROR: git-wild-hunt failed to find a github_token in the config file at {0}..exiting".format(tool_config))
+        sys.exit(1)
+    
+    # lets process the search results
+    count = 0 
+    all_leaks = []
+    for conf in results:
+        count += 1
+        log.info("processing potential leak #{0} on {1}".format(count, conf['html_url']))
+        leaks = findleaks(conf, regexes)
+        for l in leaks:
+            all_leaks.append(l)
+
+    # write output of all leaks
+    write_leaks(all_leaks, config['output'])
+        
+
+
+
+
+
+
